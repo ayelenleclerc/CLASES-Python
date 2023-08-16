0a611522ed3f36ecbe0df4741d5d819ab2503025
ayelenleclerc <ayelenleclerc@gmail.com>
2023-08-16 17:13:34 -0300

    clase 4

---
+++ clase4/ejercicio029.py
@@ -0,0 +1,28 @@
+"""Ejercicio 029
+Escribir un programa que permita Ingresar las notas de los dos parciales de 
+un alumno e indicar si promocionó, aprobó o debe recuperar. 
+Si el valor de la nota no esta entre 0 y 10, debe informar un error.
+Se promociona cuando las notas de ambos parciales son mayores o iguales a 7.
+Se aprueba cuando las notas de ambos parciales son mayores o iguales a 4.
+Se debe recuperar cuando al menos una de las dos notas es menor a 4."""
+
+nota1 = int(input("Ingrese la primera nota: "))
+nota2 = int(input("Ingrese la segunda nota: "))
+
+
+datos_ok = nota1 >= 0 and nota1 <= 10 and nota2 >= 0 and nota2 <= 10
+
+aprueba = nota1 >= 4 and nota2 >= 4
+promociona = nota1 >= 7 and nota2 >= 7
+# recupera = nota1 < 4 or nota2 < 4
+
+if datos_ok:
+    if aprueba:
+        cartel = "Aprueba"
+        if promociona:
+            cartel += " con promoción"
+    else:
+        cartel = "Reprueba"
+    print(cartel)
+else:
+    print("Error en los datos")
---
+++ clase4/ejercicio035.py
@@ -0,0 +1,26 @@
+"""Ejercicio 035
+Existen dos reglas que identifican dos conjuntos de valores:
+a) El número es de un solo dígito.
+b) El número es impar.
+A partir de estas reglas, escribir un programa que permita ingresar un número entero.
+Debe asignar el valor que corresponda a las variables booleanas:
+esDeUnSoloDigito
+esImpar
+estaEnAmbos
+noEstaEnNinguno
+el valor Verdadero o Falso, según corresponda, para indicar si el valor número ingresado pertenece o no a cada conjunto. Definir un lote de prueba de varios números y probr el algoritmo, escribiendo los resultados."""
+
+numero = int(input("Ingrese un numero: "))
+
+tiene_un_solo_digito = numero >= 0 and numero <= 9
+es_impar = (numero % 2) == 1
+esta_en_ambos = tiene_un_solo_digito and es_impar
+no_esta_en_ninguno = not esta_en_ambos
+
+print(
+    f"""
+El numero {numero} tiene un solo digito: {tiene_un_solo_digito}
+El numero {numero} es impar: {es_impar}
+El numero {numero} esta en ambos: {esta_en_ambos}
+El numero {numero} no esta en ninguno: {no_esta_en_ninguno}"""
+)
